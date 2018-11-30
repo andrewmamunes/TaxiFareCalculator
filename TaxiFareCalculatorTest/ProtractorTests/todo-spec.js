220c5ad623a9e550bd57e0aae31bdae7d94b81fd
Andrew Mamunes <andrewmamunes@aol.com>
2018-11-30 15:12:50 -0500

    Added test for reset button

--- TaxiFareCalculatorTest/ProtractorTests/todo-spec.js
+++ TaxiFareCalculatorTest/ProtractorTests/todo-spec.js
@@ -33,6 +33,33 @@
 
 
     });
+    it('should reset all fields', function () {
+        browser.get('http://localhost:50341/');
+
+        element(by.name('minutesAboveSixMph')).sendKeys(5);
+        element(by.name('milesBelowSixMph')).sendKeys(2);
+        element(by.name('startTime')).sendKeys('05');
+        element(by.name('startTime')).sendKeys('30');
+        element(by.name('startTime')).sendKeys('PM');
+        element(by.name('rideDate')).sendKeys('10-08-2010');
+        element(by.name('submitButton')).click().then(function () {
+            element(by.name('resetButton')).click().then(function () {
+                var minutesAboveSixMph = element(by.name('minutesAboveSixMph')).getAttribute('value');
+                var milesBelowSixMph = element(by.name('milesBelowSixMph')).getAttribute('value');
+                var startTime = element(by.name('startTime')).getAttribute('value');
+                var rideDate = element(by.name('rideDate')).getAttribute('value');
+                var farePrice = element(by.name('farePrice')).getAttribute('value');
+                expect(minutesAboveSixMph).toEqual('');
+                expect(milesBelowSixMph).toEqual('');
+                expect(startTime).toEqual('');
+                expect(rideDate).toEqual('');
+                expect(farePrice).toEqual('');
+            })
+        }
+    );
+
+
+    });
     it('should disable submit button because null startTime', function () {
         browser.get('http://localhost:50341/');
 
